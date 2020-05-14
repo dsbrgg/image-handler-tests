6eccc30a622f8b1fd0d65c7bb03260e69a6b22f2
dsbrgg <dsbrgg@gmail.com>
2020-05-14 11:24:36 +0100

    update js benchmark

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 const sharp = require('sharp')
 
-const rotate = (cb) => sharp('large.jpeg')
-  .rotate(90)
-  .toFile('rotate_large.jpeg', cb)
+const rotate = () => new Promise((resolve, reject) => {
+  sharp('large.jpeg')
+    .rotate(90)
+    .toFile('rotate_large.jpeg', (err, info) => {
+      if (err) reject(err)
+      resolve(info)
+    })
+})
 
 module.exports = rotate
--- js/indexBenchmark.js
+++ js/indexBenchmark.js
@@ -3,15 +3,22 @@ const { Benchmark } = require('benchmark')
 
 const suite = new Benchmark.Suite()
 
-suite.add('rotate', {
-  defer: true,
-  fn: function (deferred) {
-    for (let i = 0; i <= 100; i++) {
-      rotate(() => {
-        deferred.resolve();
-      })
+suite
+  .add('rotate', {
+    defer: true,
+    fn: async function (deferred) {
+      for (let i = 0; i <= 100; i++) {
+        await rotate()
+      }
+
+      deferred.resolve()
     }
-  }
-}).on('complete', function () {
-  console.log(this[0].stats)
-}).run()
+  })
+  .on('cycle', event => {
+    console.log(String(event.target))
+  })
+  .on('complete', function () {
+    // console.log(this[0].stats)
+  })
+
+suite.run()
